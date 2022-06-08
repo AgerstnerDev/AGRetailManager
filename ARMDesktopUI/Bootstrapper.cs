263a14bef55167bbfc4c5d98748f54530f6097bc
ARS-Andrew <94069507+ARS-Andrew@users.noreply.github.com>
2022-06-07 20:23:33 -0500

    UI config
    created initial shellview and frontend

---
+++ ARMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using ARMDesktopUI.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ARMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
